ce87e1dbbc7ca6fcc41505a82f02697e59d48ebf
mutti <imperialvictor15@gmail.com>
2024-03-05 17:39:04 +0100

    Add header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,9 +3,54 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header class="text-red-500">
-    hello
-  </header>
+  <nav class="navbar bg-base-300">
+    <div class="flex-1">
+      <RouterLink class="btn btn-ghost text-xl" to="/">
+        Exit Finder
+      </RouterLink>
 
-  <RouterView />
+      <ul class="menu menu-horizontal px-1">
+        <li>
+          <RouterLink to="/about">
+            About
+          </RouterLink>
+        </li>
+      </ul>
+    </div>
+
+    <div class="flex-none gap-2">
+      <template v-if="false">
+        <span class="font-bold">
+          John Doe
+        </span>
+        <div class="dropdown dropdown-end">
+          <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+            <div class="w-10 rounded-full">
+              <img
+                alt="Your profile picture"
+                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+              />
+            </div>
+          </div>
+          <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-300 rounded-box w-32">
+            <li>
+              <a class="text-error">
+                Logout
+              </a>
+            </li>
+          </ul>
+        </div>
+      </template>
+
+      <template v-else>
+        <a class="btn btn-primary">Login</a>
+      </template>
+    </div>
+  </nav>
+
+  <main class="flex justify-center mt-4">
+    <div class="w-4/5">
+    <RouterView />
+    </div>
+  </main>
 </template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,4 +1,3 @@
 <template>
-  <main>
-  </main>
+  Home page
 </template>
